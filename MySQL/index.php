905a91157947f884a0d77e0a5f220ec7d06c273e
Marcelo Regis <marcelo.eng93@gmail.com>
2017-08-01 02:52:24 -0300

    Configurando conexão. Adicionado connect MySQL e menssagem de error.

--- MySQL/index.php
+++ MySQL/index.php
@@ -1,5 +1,19 @@
 <?php
 
-$mtsqli = new mysqli("localhost", "root", "root", "ppa");
+$server = "localhost";
+$user = "root";
+$pass = "root";
+$database="test";
+
+//Conect MySQL
+$mysqli = new mysqli($server, $user, $pass, $database);
+
+//Error
+
+if(mysqli_connect_errno()){
+    echo "Filed to connect to MySQL:(".$mysqli->conect_errno.")".$mysqli -> connect_error;
+    exit;
+    
+}
 
 ?>
--- README.md
+++
@@ -1,2 +0,0 @@
-# englab
-Projeto para aprendizado de uma nova tecnologia e aplicação na área de civil
